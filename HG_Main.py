2cdd42ba2313ab92d649c601d0ea6eb9928f0d1d
unknown <kjl561@gmail.com>
2020-03-18 20:28:49 -0400

    origin

--- HG_Main.py
+++ HG_Main.py
@@ -27,5 +27,15 @@ def hangman():
             character_index = remaining_letters.index(guess)
             letter_board[character_index] = guess
             remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((' '.join(letter_board)))
+        print('\n'.join(stages[0: wrong_guesses + 1]))
+        if '__' not in letter_board:  
+            print('You win! The word was:')
+            print(' '.join(letter_board))
+            win = True
+            break
+        
             
 hangman()
